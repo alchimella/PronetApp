6689a4d757d9c22786fdd92cfb650a46b9b33780
Abi Radzhabova <aradzhabova@askartec.com>
2020-06-28 14:25:10 +0600

    Added re-registration user

--- src/pages/SignUpFirstStep.vue
+++ src/pages/SignUpFirstStep.vue
@@ -3,7 +3,7 @@
         <div class="flex row justify-center content-center full-width">
             <img class="l-logo" src="../assets/logo.png" />
         </div>
-        <div class="flex row justify-center content-center full-width">
+        <div v-if="!isUserExist" class="flex row justify-center content-center full-width">
             <h1 class="flex justify-center full-width">Вход</h1>
             <q-input class="full-width input" v-model="phone" type="tel" pattern="\d*" borderless unmasked-value placeholder="+996 (000) 000  000 " />
             <q-input class="q-mt-md full-width input" v-model="password" borderless :type="isPwd ? 'password' : 'text'" placeholder="Введите пароль">
@@ -17,9 +17,28 @@
             </q-input>
         </div>
 
+        <div v-else class="flex row justify-center content-center full-width">
+            <h1 class="flex justify-center full-width">Введите пароль</h1>
+            <q-input class="q-mt-md full-width input" v-model="password" borderless :type="isPwd ? 'password' : 'text'" placeholder="Введите пароль">
+                <template v-slot:append>
+                    <q-icon
+                        class="q-pr-lg"
+                        :name="!isPwd ? 'visibility_off' : 'visibility'"
+                        @click="isPwd = !isPwd"
+                    />
+                </template>
+            </q-input>
+        </div>
+
         <div class="flex row justify-center content-end items-end full-width">
             <h3 v-show="errorMessage.length > 0" class="q-pl-lg q-pr-lg q-pb-lg full-width">{{ errorMessage }}</h3>
-            <q-btn class="full-width submit-button text-white" label="Далее" :loading="submitting" :disable="!isButtonActive" @click="registerTerminal">
+            <q-btn v-if="!isUserExist" class="full-width submit-button text-white" label="Далее" :loading="submitting" :disable="!isButtonActive" @click="registerTerminal">
+                <template v-slot:loading>
+                    <q-spinner />
+                </template>
+            </q-btn>
+
+            <q-btn v-else class="full-width submit-button text-white" label="Далее" :loading="submitting" :disable="!isButtonActive" @click="reRegistration">
                 <template v-slot:loading>
                     <q-spinner />
                 </template>
@@ -42,7 +61,8 @@
                 // deviceId: device.uuid,
                 deviceId: 'cb2a8213-9da2-4756-93ea-549ae7cfe6c2',
                 signature: 'a80ef6f574652d870113226ba0cbe72c',
-                isPwd: true
+                isPwd: true,
+                isUserExist: false
             }
         },
 
@@ -70,20 +90,56 @@
                     .then(response => {
                         console.log('Первый шаг регистрацию терминала прошел успешно. Ожидается подтверждение!', response);
 
-                        let data = response.data.envelope.body.response.data;
+                        let data = response.data.envelope.body.response.data
                         let idrref = response.data.envelope.body.response.data._idrref;
 
-                        if (data.length == 0) {
-                            this.errorMessage = `Данный номер телефона уже зарегистрирован`;
+                        if (data.length != 0) {
+                            this.$config.userIdrref = idrref;
                             this.submitting = false;
                             this.isButtonActive = true;
-                            return
+                            this.$router.replace('second-step')
+                        } else {
+                            this.submitting = false;
+                            this.isButtonActive = true;
+                            this.errorMessage = `Данный номер телефона уже зарегистрирован`;
+                            this.isUserExist = true;
+                            this.password = ''
                         }
+                    })
+                    .catch(err => {
+                        console.error('Произошла ошибка при первом регистрации терминала: ', JSON.stringify(err));
 
-                        this.$config.userIdrref = idrref;
+                        this.errorMessage = 'Произошла ошибка при соединении с сервером';
                         this.submitting = false;
                         this.isButtonActive = true;
-                        this.$router.replace('second-step')
+                    });
+            },
+
+            reRegistration: function () {
+                let options = {
+                    method: 'post',
+                    url: `http://pn.pronet.kg:1072/api/81a05d419edf445b9a1d4964eade2c01?op=9&passwd=${this.password}&phone=${this.phone}`
+                };
+
+                this.$axios(options)
+                    .then(response => {
+                        console.log('Первый шаг перерегистрации пользователя прошел успешно. Ожидается подтверждение!', response);
+
+                        let idrref = response.data.envelope.body.response.data._idrref;
+                        let message = response.data.envelope.body.response.message;
+
+                        if (idrref) {
+                            this.$config.userIdrref = idrref;
+                            this.submitting = false;
+                            this.isButtonActive = true;
+                            this.isUserExist = false;
+                            this.$router.replace('second-step')
+                        } else {
+                            this.submitting = false;
+                            this.isButtonActive = true;
+                            this.errorMessage = message
+                            return
+                        }
                     })
                     .catch(err => {
                         console.error('Произошла ошибка при первом регистрации терминала: ', JSON.stringify(err));
